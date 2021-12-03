a41f5aa01e3265f168eb4d0836ad08344b108d5e
Met <memetrusidovski@gmail.com>
2021-12-02 22:40:56 -0500

    work

--- albania-travel/src/app/app.module.ts
+++ albania-travel/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
